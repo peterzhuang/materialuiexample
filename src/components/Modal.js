80466cae720e23299b32d1acb3160cb5997c73f2
yuchuan <yuchuan@yuchuan.com>
2019-11-14 08:27:37 -0500

    create Modal Comp with react portal

---
+++ src/components/Modal.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Modal = props => {
+  return ReactDOM.createPortal(
+    <div
+      onClick={() => console.log("event bubbledup!!")}
+      className="ui dimmer modals visible active"
+    >
+      <div
+        onClick={e => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
+        <div className="header">Delete Stream</div>
+        <div className="content">Are you sure you want to delete stream?</div>
+        <div className="actions">
+          <button className="ui primary button">Delete</button>
+          <button className="ui button">Cancel</button>
+        </div>
+      </div>
+    </div>,
+    document.querySelector("#modal")
+  );
+};
+
+export default Modal;
