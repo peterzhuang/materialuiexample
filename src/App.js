89e7a32bf0edf5ffc1908bb2cbb9b0472cc8c4c1
yuchuan <yuchuan@yuchuan.com>
2019-11-17 18:50:50 -0500

    Include List Component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
 import Modal from "./components/Modal";
 import ScrollDemo from "./components/ScrollDemo";
+import List from "./components/List";
 
 import "./App.css";
 
 function App() {
-
   const [shouldRender, setShouldRender] = useState(false);
 
   const handleModalUnmount = () => {
     setShouldRender(!shouldRender);
-};
+  };
 
   return (
     <div>
-      <button onClick={handleModalUnmount}>Open Modal Window</button>
-      {shouldRender && <Modal onClose={handleModalUnmount} />}
+      {/* <button onClick={handleModalUnmount}>Open Modal Window</button> */}
+      {/* {shouldRender && <Modal onClose={handleModalUnmount} />} */}
       <ScrollDemo />
+      {/* <List /> */}
     </div>
   );
 }
--- src/components/ScrollDemo.js
+++ src/components/ScrollDemo.js
@@ -1,24 +1,23 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from "react";
 
-const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetBottom);
+const scrollToRef = ref => window.scrollTo(0, ref.current.offsetTop);
 
 const ScrollDemo = () => {
-	const myRef = useRef(null)
+  const myRef = useRef(null);
 
-    useEffect(() => { 
-        scrollToRef(myRef);
-    }, []);
+  useEffect(() => {
+    scrollToRef(myRef);
+  }, []);
 
-	return (
-    <> 
-      <div style={{height:600}} />	{/* just to demonstrate scroll*/}
-      <div ref={myRef}>I wanna be seen</div> 
+  return (
+    <>
+      <div style={{ height: 600 }} /> {/* just to demonstrate scroll*/}
+      <div ref={myRef}>I wanna be seen here</div>
       {/* Attach ref object to a dom element */}
-      <div style={{height:1500}} />	{/* just to demonstrate scroll*/}
-      <button onClick={()=>scrollToRef(myRef)} >Click to scroll </button> 
+      <div style={{ height: 1500 }} /> {/* just to demonstrate scroll*/}
+      <button onClick={() => scrollToRef(myRef)}>Click to scroll </button>
     </>
-	)
-
-}
+  );
+};
 
 export default ScrollDemo;
